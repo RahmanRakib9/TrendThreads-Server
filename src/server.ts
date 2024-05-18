876e2d8dd4cb2c58ba48e5ee65462541eab45b77
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-05-18 09:34:13 +0600

    project initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from './app';
 import config from './app/config/config';
+import mongoose from 'mongoose';
 
 async function main() {
   try {
-    // await myDatabase.connect (config.database_url as string)
+    await mongoose.connect (config.database_url as string)
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
