d5600cf627a220f480d6cc37af6e11eec3e6f748
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-05-18 23:53:49 +0600

    feat:add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import flashSaleRoute from './routes/flash-sale.route';
 import categoryRoutes from './routes/category.route';
 import productRoutes from './routes/product.route';
 const app: Application = express();
+import cors from "cors"
 
 //Regular Middleware
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello From Node Typescript Clean Template Application!');
