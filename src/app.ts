b027831058319a168e8ba0e8fee844867d795c3e
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-05-22 16:28:25 +0600

    chore:host server side on vercel

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello From Node Typescript Clean Template Application!');
+  res.send('Hello From Updated Root URL!');
 });
 
 // Flash Sale Route
